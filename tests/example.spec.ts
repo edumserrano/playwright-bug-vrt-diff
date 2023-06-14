92861349cdb71f1b89ed4bc62bad0f8c32b40254
Eduardo Serrano <eduardo.serrano@lexisnexis.co.uk>
2023-06-14 01:12:42 +0100

    narrow down the problem (2)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,31 +1,62 @@
 import { expect, test } from "@playwright/test";
 import { addCoverageReport, attachCoverageReport } from "monocart-reporter";
 
-test("basic test", async ({ page }) => {
-    if (test.info().project.name === "chromium") {
-        await Promise.all([
-            page.coverage.startJSCoverage(),
-            page.coverage.startCSSCoverage(),
-        ]);
-    }
-    
+test("this shows attachments well", async ({ page }) => {
+  if (test.info().project.name === "chromium") {
+    await Promise.all([
+      page.coverage.startJSCoverage(),
+      page.coverage.startCSSCoverage(),
+    ]);
+  }
+
+  try {
     await page.goto("/");
     const filterInput = page.locator("#filter-box");
     await filterInput.fill("london");
     await expect(page).toHaveScreenshot();
-
+  } finally {
     if (test.info().project.name === "chromium") {
-        const [jsCoverage, cssCoverage] = await Promise.all([
-            page.coverage.stopJSCoverage(),
-            page.coverage.stopCSSCoverage(),
-        ]);
-        const coverageList = [...jsCoverage, ...cssCoverage];
-
-        // if the line below to attach the code coverage report is uncommented then both the snapshots
-        // and the code coverage is displayed.
-
-        // const _ = await attachCoverageReport(coverageList, test.info(), {
-        //     sourceFilter: (sourceName: any) => sourceName.search(/src\//u) !== -1,
-        // });
+      const [jsCoverage, cssCoverage] = await Promise.all([
+        page.coverage.stopJSCoverage(),
+        page.coverage.stopCSSCoverage(),
+      ]);
+      const coverageList = [...jsCoverage, ...cssCoverage];
+      const _ = await attachCoverageReport(coverageList, test.info(), {
+        sourceFilter: (sourceName: any) => sourceName.search(/src\//u) !== -1,
+      });
     }
+  }
+});
+
+test("this only shows code coverage", async ({ page }) => {
+  if (test.info().project.name === "chromium") {
+    await Promise.all([
+      page.coverage.startJSCoverage(),
+      page.coverage.startCSSCoverage(),
+    ]);
+  }
+
+  await page.goto("/");
+  const filterInput = page.locator("#filter-box");
+  await filterInput.fill("london");
+
+  if (test.info().project.name === "chromium") {
+    const [jsCoverage, cssCoverage] = await Promise.all([
+      page.coverage.stopJSCoverage(),
+      page.coverage.stopCSSCoverage(),
+    ]);
+    const coverageList = [...jsCoverage, ...cssCoverage];
+    const _ = await attachCoverageReport(coverageList, test.info(), {
+      sourceFilter: (sourceName: any) => sourceName.search(/src\//u) !== -1,
+    });
+  }
+
+  await expect(page).toHaveScreenshot();
+});
+
+test("this does not show any attachment", async ({ page }) => {
+  await page.goto("/");
+  const filterInput = page.locator("#filter-box");
+  await filterInput.fill("london");
+  await expect(page).toHaveScreenshot();
 });
